f6a2d06f150c1f19f02fc0d5de4ac72f21e6d37b
MOHAMEDJJJJAK <52308633+MOHAMEDJJJJAK@users.noreply.github.com>
2019-07-10 12:17:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3524,4 +3524,4 @@ client.on('guildMemberAdd', member => {
 return channel.send("")
     }
     )});
-client.login(process.env.BOT_TOKEN)
+client.login('NTk4MTA4NjQ3ODkyOTc1NjM4.XSW2qw.zH1En4_8uDpiNGroRQffLUXkN_Q')
